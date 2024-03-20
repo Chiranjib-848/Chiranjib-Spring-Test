a89647334cbd64d8f600b64fbe3b3711189323ab
Chiranjib-848 <chiranjib.innowave@gmail.com>
2024-03-20 22:42:31 +0530

    Base Spring-boot project

---
+++ base-springboot-app/src/test/java/com/luv2code/springboot/demo/mycoolapp/MycoolappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luv2code.springboot.demo.mycoolapp;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycoolappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
